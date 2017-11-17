c9b7f5e3415294f925ebc2dde93748da3daff67b
Matt Dalton <matthew.dalton@porism.com>
2017-11-17 18:04:44 +0000

    not working

--- mathsTest.js
+++ mathsTest.js
@@ -1,11 +1,14 @@
 const mathsTools = require('./js/mathsTools');
 
 let r = [];
-let x = 30000;
+let x = 100000;
 for (let i = 0; i < 100000; i++) {
     r.push(mathsTools.obfuscate(0 + i));
 }
 
+console.log(r);
+
+/*
 const m = {};
 
 r.map((v, i) => { m[v] = (m[v] || []); m[v].push(i); });
@@ -14,5 +17,6 @@ for (let num in m) {
     if (m[num].length > 1)
         console.log(num + ' : ' + m[num]);
 }
+*/
 
 console.log(mathsTools.restore(mathsTools.obfuscate(234634)));
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const mathsTools = require('./js/mathsTools.js');
+const data = require('./js/data.js');
 
 
 app.get('/', (request, response) => response.sendFile('index.html', { root: __dirname + '\\public' } ))
@@ -9,8 +10,15 @@ app.get('/', (request, response) => response.sendFile('index.html', { root: __di
 
     .get('/obfuscate/:number', (req, res) => res.json({
         number: req.params.number,
-        obfuscated: mathsTools.obfuscate(req.params.number)
+        code: mathsTools.obfuscate(req.params.number)
     }))
+
+    .get('/restore/:code', (req, res) => res.json({
+        code: req.params.code,
+        number: mathsTools.restore(req.params.code)
+    }))
+
+    .get('/url/get/:code', (req, res) => data.url.get())
 
     .get('/sample/text', (request, response) => response.send('<ul><li>a</li><li>b</li><li>c</li></ul>'))
     .get('/sample/json', (request, response) => response.json(['a', 'b', 'c']))
